c1e24596c13aa1d55284fbfc592e1cfb43c5007e
Allen Roman <allen@allenroman.com>
2020-11-20 12:01:53 -0800

    Collision detection for all displayed objects

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+io.stdout:setvbuf("no")
 require("character")
 require("npcs")
 
@@ -11,21 +12,13 @@ function love.load()
 end
 
 
+-- Store characters old position at all times
+-- Make a movement
+-- If movement would collide, bring them back to old position?
+
 function love.update(dt)
-	-- print(character.x)
 
-
-	if love.keyboard.isDown("right") and character.x < love.graphics.getWidth() - character.width then
-		character.x = character.x + 200 * dt
-	elseif love.keyboard.isDown("left")  and character.x > 0 then
-		character.x = character.x - 200 * dt	
-	elseif love.keyboard.isDown("up") and character.y > 0 then
-		character.y = character.y - 200 * dt	
-	elseif love.keyboard.isDown("down") and character.y < love.graphics.getHeight() - character.height then
-		character.y = character.y + 200 * dt
-	end
-
-
+	characterMovement(dt)
 
 	if love.keyboard.isDown("escape") then
 		love.event.quit()
@@ -38,6 +31,7 @@ function love.draw()
 	love.graphics.rectangle(mode, character.x, character.y, character.width, character.height)
 
 	for i,v in ipairs(listOfRectangles) do
+		print("Drawing " .. i)
 		if checkCollision(character, v) then
 			mode = "fill"
 		else
@@ -47,13 +41,64 @@ function love.draw()
 	end
 end
 
+function checkCharacterCollisions()
+	for i,v in ipairs(listOfRectangles) do
+		if checkCollision(character, v) then
+			print(i .. " is colliding")
+			return true
+		else
+			print(i .. " not colliding!")
+
+			return false
+		end
+	end
+end
+
+function characterMovement(dt)
+	if love.keyboard.isDown("right") and character.x < love.graphics.getWidth() - character.width then
+		previousX = character.x
+		character.x = character.x + 200 * dt
+		for i,v in ipairs(listOfRectangles) do
+			if checkCollision(character, v) then
+				character.x = previousX
+			end
+		end
+	elseif love.keyboard.isDown("left")  and character.x > 0 then
+		previousX = character.x
+		character.x = character.x - 200 * dt
+		for i,v in ipairs(listOfRectangles) do
+			if checkCollision(character, v) then
+				character.x = previousX
+			end
+		end
+	elseif love.keyboard.isDown("up") and character.y > 0  then
+		previousY = character.y
+		character.y = character.y - 200 * dt
+		for i,v in ipairs(listOfRectangles) do
+			if checkCollision(character, v) then
+				character.y = previousY
+			end
+		end
+	elseif love.keyboard.isDown("down") and character.y < love.graphics.getHeight() - character.height then
+		previousY = character.y
+		character.y = character.y + 200 * dt
+		for i,v in ipairs(listOfRectangles) do
+			if checkCollision(character, v) then
+				character.y = previousY
+			end
+		end
+	end
+end
+
+
+
 
 function checkCollision(a, b)
 	local a_left = a.x
 	local a_right = a.x + a.width
 	local a_top = a.y
 	local a_bottom = a.y + a.height
-	
+
 	local b_left = b.x
 	local b_right = b.x + b.width
 	local b_top = b.y
@@ -68,3 +113,5 @@ function checkCollision(a, b)
 		return false
 	end
 end
+
+
